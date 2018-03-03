21a6ea2b9a8bea0330a4f62789be62a07ec4ab1e
zbasper <asper@21cn.com>
2018-03-03 15:51:15 +0800

    Update ConvertToTFRecord.py

--- tianwen/ConvertToTFRecord.py
+++ tianwen/ConvertToTFRecord.py
@@ -1,8 +1,29 @@
 
+import tensorflow as tf
+import zipfile as zf
+import argparse
 
 data_path = "eeeeee.zip"
 label_path = "sefsef.csv"
 labels = {'star': 0, 'galaxy': 1, 'qso': 2, 'unknown': 3}
 
-def load_data(batch):
-    df = pd.read
+
+
+
+def main(args):
+    
+    tfRecordOption = tf.python_io.TFRecordOptions(tf.python_io.TFRecordCompressionType.ZLIB)
+    writer = tf.python_io.TFRecordWriter("xxxxx.tfrecord", tfRecordOption)
+    
+    with zf.ZipFile(data_path, "r") as myzip:
+    
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Process some parameters.")
+    parser.add_argument("--dataNum", type=int)
+    args = parser.parse_args()
+    
+    main(args)
+    
