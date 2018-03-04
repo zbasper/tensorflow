a0e310871954c3aba94125cee05b981d4e379ca1
zbasper <asper@21cn.com>
2018-03-04 08:46:01 +0800

    Update tianwen_estimator.py

--- tianwen/tianwen_estimator.py
+++ tianwen/tianwen_estimator.py
@@ -3,3 +3,15 @@ import tensorflow as tf
 
 def input_evaluation_set():
 
+def main():
+    
+    
+    
+    
+    classifer = tf.estimator.DNNClassifier(
+        feature_columns = my_feature_columns,
+        hidden_units=[10, 10],
+        n_classes=3
+    )
+    
+    
