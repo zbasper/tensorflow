12ff8a8d99a0b8fe12973aae1f62c12569b22c68
zbasper <asper@21cn.com>
2018-03-08 22:58:04 +0800

    Update tianwen_estimator.py

--- tianwen/tianwen_estimator.py
+++ tianwen/tianwen_estimator.py
@@ -8,6 +8,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=100, type=int, help="batch size")
 parser.add_argument("--train_steps", default=1000, type=int, help="number of training steps")
 
+TRAIN_FILE = "tianwen_data_zlib.tfrecord"
+TEST_FILE = "tianwen_data_eval_zlib.tfrecord"
+PREDICT_FILE = "tianwen_data_test_zlib.tfrecord"
 
 def input_evaluation_set():
 
@@ -17,6 +20,7 @@ def main():
     batch_size = args.batch_size
     train_steps = args.train_steps
     
+    
     # Build 2 hidden layer DNN with 10, 10 units respectively.
     classifer = tf.estimator.DNNClassifier (
         feature_columns = my_feature_columns,
@@ -24,26 +28,33 @@ def main():
         n_classes = 3
     )
     
+    dataset_train = tianwen_data.load_data(TRAIN_FILE)
     # Train the model
     classifier.train (
-        input_fn = tianwen_data.train_input_fn(),
+        input_fn = lambda: tianwen_data.train_input_fn(dataset_train, batch_size),
         steps = train_steps
     )
     
+    dataset_test = tianwen_data.load_data(TEST_FILE)
     #Evaluate the model.
     eval_result = classifier.evaluate (
-        input_fn = tianwen_data.eval_input_fn()
+        input_fn = lambda: tianwen_data.eval_input_fn(dataset_test, batch_size)
     )
-    
     print("Test set accuracy: (accuracy:0.3)\n".format(**eval_result))
     
+    dataset_predict = tianwen_data.load_data(PREDICT_FILE)
     #Generate predictions from the model
-    
     predictions = classifier.predict (
-        input_fn = tianwen_data.eval_input_fn()
+        input_fn = lambda: tianwen_data.eval_input_fn(dataset_predict)
     )
     
+    for pred_dict, expec in zip(predictions, expected):
+        template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
 
+        class_id = pred_dict['class_ids'][0]
+        probability = pred_dict['probabilities'][class_id]
+
+        
 if __name__ == "__main__":
     tf.logging.set_verbosity(tf.loggin.INFO)
     tf.app.run(main)
